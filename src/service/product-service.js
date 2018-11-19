29beb7936e68ce8d65d34dbda8dd9c3a9ed586d1
visuper <visuperme@gmail.com>
2018-11-19 11:11:27 +0800

    update code

---
+++ src/service/product-service.js
@@ -0,0 +1,34 @@
+/*
+* @Author: visuper
+* @Date:   2017-09-27 18:26:52
+* @Last Modified by:   visuper
+* @Last Modified time: 2018-11-19 10:59:15
+*/
+
+'use strict';
+
+var _mm = require('util/mm.js');
+
+var _product = {
+    // 获取商品列表
+    getProductList : function(listParam, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/product/list.do'),
+            data    : listParam,
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取商品详细信息
+    getProductDetail : function(productId, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/product/detail.do'),
+            data    : {
+                productId : productId
+            },
+            success : resolve,
+            error   : reject
+        });
+    }
+}
+module.exports = _product;
---
+++ src/util/slider/index.js
@@ -0,0 +1,11 @@
+/*
+* @Author: visuper
+* @Date:   2017-09-26 18:19:14
+* @Last Modified by:   visuper
+* @Last Modified time: 2018-11-19 11:01:22
+*/
+
+'use strict';
+
+require('./index.css');
+require('./unslider.js');
